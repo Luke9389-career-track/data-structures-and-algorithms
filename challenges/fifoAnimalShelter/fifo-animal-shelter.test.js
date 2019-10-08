6b428a3b77f548c59d8166bad124a93404470871
Luke Donahue <luke9389@gmail.com>
2019-10-08 16:58:33 -0700

    havent tested yet

---
+++ challenges/fifoAnimalShelter/fifo-animal-shelter.test.js
@@ -0,0 +1,11 @@
+const { AnimalShelter } = require('./fifo-animal-shelter');
+
+
+describe('Linked list', () => {
+  
+  it('can enqueue an animal', () => {
+    const shelter = new AnimalShelter;
+    
+  });
+
+});
