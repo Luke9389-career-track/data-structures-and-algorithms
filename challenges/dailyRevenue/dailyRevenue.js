9df5312fe40540d5525e743dabaa2fca055e5f61
Luke Donahue <luke9389@gmail.com>
2019-10-23 15:46:31 -0700

    dailyRevenue refactor and inclusion of timezone

--- challenges/dailyRevenue/dailyRevenue.js
+++ challenges/dailyRevenue/dailyRevenue.js
@@ -1,13 +1,13 @@
 function dailyRevenue(transactions) {
-  const summary = {};
-  
-  transactions.forEach(transaction => {
+
+  return transactions.reduce((acc, transaction) => {
     //                        Takes out the hours mins and secs   Time Zone
     //transaction.timestamp - (transaction.timestamp % 86400000) + 25200000
-    const date = new Date(transaction.timestamp - (transaction.timestamp % 86400000) + 25200000);
-    summary[date.toString().slice(0, 15)] ? summary[date.toString().slice(0, 15)] += transaction.price : summary[date.toString().slice(0, 15)] = transaction.price;
-  });
-  return summary;
+    const date = new Date(transaction.timestamp - (transaction.timestamp % 86400000) + (new Date().getTimezoneOffset() * 60000));
+    acc[date.toString().slice(0, 15)] ? acc[date.toString().slice(0, 15)] += transaction.price : acc[date.toString().slice(0, 15)] = transaction.price;
+    return acc;
+  }, {});
+
 }
 //8.64 x 10^7 milliseconds in a day
 module.exports = {
