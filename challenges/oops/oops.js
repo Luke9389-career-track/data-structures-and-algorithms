259b22d43ecc5181ec738cc4ae93fdbe6209aa90
Luke Donahue <luke9389@gmail.com>
2019-10-29 16:46:22 -0700

    finished!

--- challenges/oops/oops.js
+++ challenges/oops/oops.js
@@ -1,5 +1,32 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 
-fs.readdirSync('./challenges/oops/data/').forEach(file => {
-  console.log(fs.readFileSync(`./challenges/oops/data/${file}`).toString());
-});
+function oops(path) {
+  fs.readdir(path)
+    .then(files => {
+      return Promise.all(
+        files.map(file => {
+          return Promise.all([
+            file,
+            fs.readFile(`${path}${file}`),
+            file.split('.')[0],
+            fs.stat(`${path}${file}`)
+          ]);
+        })
+      );
+    })
+    .then(dataArr => {
+      dataArr.forEach(data => {
+        fs.rename(`${path}${data[0]}`, `${path}${data[1]}-${data[2]}-${data[3].mtime.toJSON()}`);
+      });
+    });
+}
+
+oops('./challenges/oops/data/');
+
+module.exports = {
+  oops
+};
+
+
+
+
