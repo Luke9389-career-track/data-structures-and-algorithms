8897d2736695c4c52f5d5be30f7898ed6de31828
Luke Donahue <luke9389@gmail.com>
2019-11-09 14:45:37 -0800

    completed binary tree challenge

--- challenges/tree/tree.js
+++ challenges/tree/tree.js
@@ -1,24 +1,49 @@
 class Node {
-  constructor(value){
+  constructor(value) {
     this.value = value;
-    this.next = null;
+    this.left = null;
+    this.right = null;
   }
 }
 
 module.exports = class Tree {
-  constructor() {
-    this.root = null;
+  constructor(value = null) {
+    value ? this.root = new Node(value) : this.root = null;
   }
 
   add(value) {
+    if(!this.root) { this.root = new Node(value); return; }
     let node = this.root;
-    return recurse(value, node);
+    return findAndDrop(value, node);
+  }
+
+  preOrderTraversal(node = this.root, values = []) {
+    values.push(node.value);
+    if(node.left) this.preOrderTraversal(node.left, values);
+    if(node.right) this.preOrderTraversal(node.right, values);
+    return values;
+  }
+
+  inOrderTraversal(node = this.root, values = []) {
+    if(node.left) this.inOrderTraversal(node.left, values);
+    values.push(node.value);
+    if(node.right) this.inOrderTraversal(node.right, values);
+    return values;
+  }
+
+  postOrderTraversal(node = this.root, values = []) {
+    if(node.left) this.postOrderTraversal(node.left, values);
+    if(node.right) this.postOrderTraversal(node.right, values);
+    values.push(node.value);
+    return values;
   }
 };
 
-function recurse(value, node) {
+function findAndDrop(value, node) {
   let dir = '';
-  value > node.val ? dir = 'right' : dir = 'left';
-  return node[dir] ? recurse(value, node[dir]) : node[dir] = new Node(value);
+  value > node.value ? dir = 'right' : dir = 'left';
+  return node[dir] ? findAndDrop(value, node[dir]) : node[dir] = new Node(value);
 }
+
+
 
