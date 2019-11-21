a43a698a702cb641538ef64314f1f5937e8933ca
Luke Donahue <luke9389@gmail.com>
2019-11-21 15:57:52 -0800

    quick sort works! (holy guacamole that was hard)

--- challenges/mergeSort/mergeSort.test.js
+++ challenges/mergeSort/mergeSort.test.js
@@ -3,8 +3,6 @@ const { mergeSort } = require('./mergeSort.js');
 describe('merge sort module', () => {
   it('example 1', () => {
     const array1 = [8, 23, 4, 42, 16, 15];
-    console.log(mergeSort(array1));
     expect(mergeSort(array1)).toEqual([4, 8, 15, 16, 23, 42]);
-
   });
 });
